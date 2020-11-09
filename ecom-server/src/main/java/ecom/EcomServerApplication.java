85628798e41654066a12661a835cbeeadd0638bf
najat-el <najatelhabti1997@gmail.com>
2020-11-09 19:16:50 +0100

    first commit

---
+++ ecom-server/src/main/java/ecom/EcomServerApplication.java
@@ -0,0 +1,13 @@
+package ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomServerApplication.class, args);
+    }
+
+}
---
+++ ecom-server/src/test/java/ecom/EcomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
