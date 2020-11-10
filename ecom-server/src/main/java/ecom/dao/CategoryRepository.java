8acdd1a7a5d3bcd3142d62b63d52ce6ffe495f91
najat-el <najatelhabti1997@gmail.com>
2020-11-10 20:55:06 +0100

    add catalogue service

--- ecom-server/src/main/java/ecom/dao/CategoryRepository.java
+++ ecom-server/src/main/java/ecom/dao/CategoryRepository.java
@@ -3,7 +3,9 @@ package ecom.dao;
 import ecom.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @RepositoryRestResource
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
