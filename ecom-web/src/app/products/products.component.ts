e21d0d5124ea140c8c8ec5e62a555f7b629c249c
najat-el <najatelhabti1997@gmail.com>
2020-11-12 20:33:56 +0100

    add products component

--- ecom-web/src/app/products/products.component.ts
+++ ecom-web/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CatalogueService} from '../catalogue.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  public products;
 
-  constructor() { }
+  constructor(private catalogueService:CatalogueService) { }
 
   ngOnInit(): void {
+    this.getProducts()
   }
 
+  private getProducts() {
+    this.catalogueService.getResource("/products/search/selectedProducts")
+      .subscribe(data=>{
+        this.products=data;
+      },error => console.log(error))
+  }
 }
